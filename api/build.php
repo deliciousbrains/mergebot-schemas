60c4b84d0216f59118fef8b754c36b5b764a8720
Iain <iain@polevaultweb.com>
2017-12-06 13:48:30 +0000

    Support themes api file. Closes https://github.com/deliciousbrains/mergebot-schemas/issues/137

--- api/build.php
+++ api/build.php
@@ -1,33 +1,38 @@
 <?php
 
-$schema_dir = dirname( __DIR__ ) . '/plugins/';
-$plugins = array_diff( scandir( $schema_dir), array( '..', '.' ) );
+function build_api_for_schemas( $type = 'plugins' ) {
+	$schema_dir = dirname( __DIR__ ) . '/' . $type . '/';
+	$schemas    = array_diff( scandir( $schema_dir ), array( '..', '.' ) );
 
-$plugins_index = array();
+	$plugins_index = array();
 
-foreach( $plugins as $plugin  ) {
-	$file = $schema_dir . $plugin;
-	$contents = file_get_contents( $file );
-	$schema   = json_decode( $contents );
+	foreach ( $schemas as $schema_file ) {
+		$file     = $schema_dir . $schema_file;
+		$contents = file_get_contents( $file );
+		$schema   = json_decode( $contents );
 
-	if ( ! isset( $schema->name ) ) {
-		continue;
+		if ( ! isset( $schema->name ) ) {
+			continue;
+		}
+
+		$in_git = exec( sprintf( 'git status %s', $file ) );
+		if ( 'nothing added to commit but untracked files present (use "git add" to track)' === $in_git ) {
+			// Schema not committed, WIP
+			continue;
+		}
+
+		$plugins_index[ $schema_file ] = array(
+			'name'       => $schema->name,
+			'version'    => $schema->version,
+			'testedUpTo' => $schema->testedUpTo,
+			'pluginURL'  => $schema->url,
+			'schemaURL'  => 'https://github.com/deliciousbrains/mergebot-schemas/blob/master/' . $type . '/' . $schema_file,
+		);
 	}
 
-	$in_git = exec( sprintf( 'git status %s', $file ) );
-	if ( 'nothing added to commit but untracked files present (use "git add" to track)' === $in_git ) {
-		// Schema not committed, WIP
-		continue;
-	}
-
-	$plugins_index[ $plugin ] = array(
-		'name'       => $schema->name,
-		'version'    => $schema->version,
-		'testedUpTo' => $schema->testedUpTo,
-		'pluginURL'  => $schema->url,
-		'schemaURL'  => 'https://github.com/deliciousbrains/mergebot-schemas/blob/master/plugins/' . $plugin,
-	);
+	$index_file = dirname( __FILE__ ) . '/' . $type . '.json';
+	file_put_contents( $index_file, json_encode( $plugins_index, JSON_PRETTY_PRINT ) );
 }
 
-$index_file = dirname( __FILE__ ) . '/plugins.json';
-file_put_contents( $index_file, json_encode( $plugins_index, JSON_PRETTY_PRINT ) );
+build_api_for_schemas( 'plugins' );
+build_api_for_schemas( 'themes' );
